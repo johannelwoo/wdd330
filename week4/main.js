eebf8d74db36840a8f0f579efae097f4cf8fd9ad
Johanna Schick <77026865+johannelwoo@users.noreply.github.com>
2021-06-15 22:49:06 -0600

    portfolio

---
+++ week4/main.js
@@ -0,0 +1,9 @@
+
+​
+import Hikes from './hikes.js';
+​
+// create an instance of the class
+const myHike = new Hikes('hikes');
+​
+// use instance to call showHikeList method
+myHike.showHikeList();
